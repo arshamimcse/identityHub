f94ee11d47bf72c14b5aa666fea22d49d187343a
arshamimcse <arshamim.cse@gmail.com>
2024-12-01 19:57:06 +0600

    Initial commit

---
+++ assetes/js/script.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
+});
+
+
+document.querySelector('.custom-next-button').addEventListener('click', () => {
+  swiper.slideNext();
+});
+
+document.querySelector('.custom-prev-button').addEventListener('click', () => {
+  swiper.slidePrev();
+});
+
+
+
+const menuList = document.querySelector(".nav-links")
+function toggleMenu(){
+    menuList.classList.toggle('show-navigation')
+}
